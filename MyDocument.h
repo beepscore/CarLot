1c0b29b65d6506c8e4e8863b15c4b696ac552c8c
beepscore <support@beepscore.com>
2009-11-09 10:38:31 -0800

    Initial commit.  Ref Hillegass Ch 11.

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  CarLot
+//
+//  Created by Steve Baker on 11/9/09.
+//  Ref Hillegass, Cocoa Programming for Mac OS X, 3rd edition, Ch 11
+//  Copyright Beepscore LLC 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CarLot
+//
+//  Created by Steve Baker on 11/9/09.
+//  Copyright Beepscore LLC 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}

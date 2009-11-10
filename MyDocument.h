41a2da0a1589b9c4bcb4290531206e687d70fa2e
beepscore <support@beepscore.com>
2009-11-10 14:46:58 -0800

    In IB, create UI and bind values.  Ref Hillegass pg 173 - 180.
    TODO: Don't bind image.  Data binding deprecated in Mac OS X 10.6
    http://forums.macrumors.com/showthread.php?t=781991
    Post #7 suggests using value transformers and some code.

--- MyDocument.h
+++ MyDocument.h
@@ -6,6 +6,10 @@
 //  Ref Hillegass, Cocoa Programming for Mac OS X, 3rd edition, Ch 11
 //  Copyright Beepscore LLC 2009 . All rights reserved.
 //
+// TODO: Don't bind image.  Data binding deprecated in Mac OS X 10.6
+// http://forums.macrumors.com/showthread.php?t=781991
+// Post #7 suggests using value transformers and some code.
+
 
 #import <Cocoa/Cocoa.h>
 
